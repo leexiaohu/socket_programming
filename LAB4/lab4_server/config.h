2c354312514c038c0034890a4688b83bbcd2cd04
leexiaohu <leexiaohu@live.com>
2016-06-28 11:35:32 +0800

    add lab4

---
+++ LAB4/lab4_server/config.h
@@ -0,0 +1,23 @@
+//define some micros
+
+#define NUM_THREADS 64
+#define QUEUE_LENGTH 30
+#define SERVER_NAME "Nap"
+#define DEFAULT_PORT "8989"
+#define PRIVILEGED_PORT 7898
+#define MAX_PORT 65535
+#define TP_UTIL 128
+
+#define ERROR_MSG "Error"
+#define OK_MSG "OK"
+#define INFO_MSG "INFO"
+#define WARN_MSG "Warning"
+#define USER_MSG "User"
+
+#define DB_FILE "data_mp3.db"
+//#define SQLITE_OK 0
+//#define SQLITE_ERROR 1
+//#define SQLITE_CONSTRAINT 19
+//#define SQLITE_DONE 21
+
+
